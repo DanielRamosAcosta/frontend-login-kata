fd570c57face15b9014ee01bda5e312a01977d26
Daniel Ramos <danielramosacosta1@gmail.com>
2024-03-14 20:37:50 +0100

    unify react and vue

--- src/pages/SignUp.test.jsx
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest";
-
-describe("signup", () => {
-  it("redirects to the success page when signup is ok", async () => {
-    expect(true).toBe(true);
-  });
-});
