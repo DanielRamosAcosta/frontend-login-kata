ffbd0968359eeb1f14c7e002d785a3ea7aab3f61
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-06 19:53:08 +0200

    update js react CI

---
+++ js-react/playwright.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const port = 5173;
+
+export default defineConfig({
+  testDir: "./e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: `http://localhost:${port}`,
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+  ],
+  webServer: {
+    command: `npx vite --port ${port}`,
+    timeout: 10 * 1000,
+    port,
+    reuseExistingServer: !process.env.CI,
+  },
+});
