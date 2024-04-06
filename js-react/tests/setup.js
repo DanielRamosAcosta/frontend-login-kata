627c64163ccb458aa8239e809cf166452815c067
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-06 21:53:08 +0200

    fix import of vitest

--- js-react/tests/setup.js
+++ js-react/tests/setup.js
@@ -1,6 +1,1 @@
-import { expect } from "vitest";
-import fetch from "node-fetch";
-import matchers from "@testing-library/jest-dom/matchers";
-
-expect.extend(matchers);
-global.fetch = fetch;
+import "@testing-library/jest-dom/vitest";
