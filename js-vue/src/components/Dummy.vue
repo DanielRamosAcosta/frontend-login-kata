6856caede621e3110bec993ed189007c552a420d
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-06 21:25:05 +0200

    add dummy component and test

---
+++ js-vue/src/components/Dummy.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="dummy">
+    <p>Counter value: {{ counter }}</p>
+    <button @click="increment">Increment</button>
+    <input v-model="text" placeholder="Type here" />
+    <p>Text value: {{ text }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+const text = ref("");
+
+function increment() {
+  counter.value++;
+}
+</script>
+
+<style scoped></style>
