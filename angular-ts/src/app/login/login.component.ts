d5abe302253edebd5c56166b1b2d0d47e5e583b4
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-18 23:35:06 +0200

    add recipes

--- angular-ts/src/app/login/login.component.ts
+++ angular-ts/src/app/login/login.component.ts
@@ -12,11 +12,37 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   email = '';
   password = '';
+  isLoading = false;
+  errorMessage = '';
 
   constructor(private readonly router: Router) {}
 
   async login() {
-    console.log(this.email, this.password);
-    await this.router.navigateByUrl('/recipes');
+    return fetch('https://backend-login-placeholder.deno.dev/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email: this.email, password: this.password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'error') {
+          throw new Error(data.code);
+        }
+        return data.payload;
+      })
+      .then((payload) => {
+        localStorage.setItem('token', payload.jwt);
+      })
+      .then(() => {
+        return this.router.navigateByUrl('/recipes');
+      })
+      .catch((error) => {
+        this.errorMessage = error.message;
+      })
+      .finally(() => {
+        this.isLoading = false;
+      });
   }
 }
--- angular-ts/src/app/recipes/recipes.component.ts
+++ angular-ts/src/app/recipes/recipes.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecipeCardComponent } from '../recipe-card/recipe-card.component';
+import { NgFor } from '@angular/common';
+
+type Recipe = {
+  id: string;
+  name: string;
+  ingredients: string[];
+};
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [RecipeCardComponent, NgFor],
   templateUrl: './recipes.component.html',
-  styleUrl: './recipes.component.css'
+  styleUrl: './recipes.component.css',
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit {
+  recipes: Recipe[] = [];
 
+  ngOnInit(): void {
+    fetch('https://backend-login-placeholder.deno.dev/api/recepies', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === 'error') {
+          throw new Error(data.code);
+        }
+        return data.payload;
+      })
+      .then((recipes) => {
+        this.recipes = recipes;
+      });
+  }
 }
