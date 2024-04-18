fbcf84e3b30e08a9fcbd682388d1f2ebe08f0aab
Daniel Ramos <danielramosacosta1@gmail.com>
2024-04-18 23:35:15 +0200

    add new components

---
+++ angular-ts/src/app/recipe-card/recipe-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-recipe-card',
+  templateUrl: './recipe-card.component.html',
+  styleUrl: './recipe-card.component.css',
+  imports: [NgFor],
+})
+export class RecipeCardComponent {
+  @Input() name: string = '';
+  @Input() ingredients: string[] = [];
+}
